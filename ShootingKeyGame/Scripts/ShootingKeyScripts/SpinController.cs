e5de336c9607706a0c09df770391525dc854d983
TriNHM <triminh0502@gmail.com>
2025-03-09 10:07:25 +0700

    add shooting key game with structured folder

---
+++ ShootingKeyGame/Scripts/ShootingKeyScripts/SpinController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpinController : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 1f;
+    private bool isRight = true;
+
+    public int minSpeed;
+    public int maxSpeed;
+
+    private ShootingKeyAudioController audioController;
+
+    private void Awake()
+    {
+        audioController = FindAnyObjectByType<ShootingKeyAudioController>();
+    }
+
+    void Start()
+    {
+        StartCoroutine(CheckDirectionChange()); // Start checking direction every 5s
+    }
+
+    void Update()
+    {
+        Spin();
+    }
+
+    void Spin()
+    {
+        transform.Rotate((isRight ? 1 : -1) * Vector3.forward * rotationSpeed * Time.deltaTime);
+    }
+
+    IEnumerator CheckDirectionChange()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(5f);
+            rotationSpeed = RandomRotationSpeed();
+            isRight = RandomLeftRight();
+        }
+    }
+    float RandomRotationSpeed()
+    {
+        var r =  Random.Range(minSpeed, maxSpeed);
+        if(r > 150)
+            rotationSpeed = r;
+       return rotationSpeed;
+    }
+
+    bool RandomLeftRight()
+    {
+        return Random.Range(0, 1) > 0.5f;
+    }
+}
