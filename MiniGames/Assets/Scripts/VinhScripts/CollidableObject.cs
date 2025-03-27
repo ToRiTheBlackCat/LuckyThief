3d50bea86071508fa0e5ad055424c197b3dd82e8
SentientPeas <sentientPeas@example.com>
2025-03-27 09:18:43 +0700

    Merge Vinh

---
+++ MiniGames/Assets/Scripts/VinhScripts/CollidableObject.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace LuckyThief.VinhScripts
+{
+
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class CollidableObject : MonoBehaviour
+    {
+        private Collider2D objectCollider;
+        [SerializeField] //expose private fields
+        private ContactFilter2D filter;
+        private List<Collider2D> collidedObjects = new List<Collider2D>(1);
+
+        protected virtual void Start()
+        {
+            objectCollider = GetComponent<Collider2D>();
+            if (objectCollider == null)
+            {
+                Debug.LogError("Collider2D component is missing on this GameObject.");
+            }
+        }
+
+        protected virtual void Update()
+        {
+            if (objectCollider != null)
+            {
+                objectCollider.Overlap(filter, collidedObjects);
+                foreach (var o in collidedObjects)
+                {
+                    OnCollided(o.gameObject);
+                }
+            }
+        }
+
+        protected virtual void OnCollided(GameObject collidedObject)
+        {
+            Debug.Log("Collided with " + collidedObject.name);
+        }
+    }
+
+}
